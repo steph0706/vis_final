eb1ab08bcf6b5e2adf71218b81f88b9b2ccd53da
chentetran <vincenttran23@gmail.com>
2017-12-01 16:47:11 -0500

    centered map

--- languages/src/App.js
+++ languages/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactMapGL from 'react-map-gl';
+import InteractiveMap from 'react-map-gl';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoidnRyYW4wMSIsImEiOiJjamFvZXcwbXAwaDNkMzNwZm01eG10MHhkIn0.HMWFx0t9PAyxpG0EV6P6lg';
 
@@ -15,18 +15,21 @@ class App extends Component {
 The scientific study of language is called linguistics. Questions concerning the philosophy of language, such as whether words can represent experience, have been debated since Gorgias and Plato in ancient Greece. Thinkers such as Rousseau have argued that language originated from emotions while others like Kant have held that it originated from rational and logical thought. 20th-century philosophers such as Wittgenstein argued that philosophy is really the study of language. Major figures in linguistics include Ferdinand de Saussure and Noam Chomsky.
         </p>
 
-        <ReactMapGL
-        flex= {1}
-        
-        latitude={0}
-        longitude={0}
-        zoom={0}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-        onViewportChange={(viewport) => {
-          const {width, height, latitude, longitude, zoom} = viewport;
-          // Optionally call `setState` and use the state to update the map.
-        }}
-      />
+        <InteractiveMap
+	        className="center"
+	        // width={1600}
+	        flex={1}
+	        height={600}
+	        latitude={0}
+	        longitude={0}
+	        zoom={1}
+	        mapStyle="mapbox://styles/mapbox/dark-v9"
+	        mapboxApiAccessToken={MAPBOX_TOKEN}
+	        onViewportChange={(viewport) => {
+	          const {width, height, latitude, longitude, zoom} = viewport;
+	          // Optionally call `setState` and use the state to update the map.
+	        }}
+	      />
       </div>
     );
   }
