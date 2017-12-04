4dc37025d133f8b8e05b004ea6cca3c073f2cb5f
chentetran <vincenttran23@gmail.com>
2017-12-04 16:12:35 -0500

    first commit; treemap attempt

--- languages/src/App.js
+++ languages/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import {
+	XYPlot, XAxis, YAxis, 
+	HorizontalGridLines, LineSeries,
+	Treemap
+} from 'react-vis';
 import './App.css';
 import ReactMapGL, {Popup} from 'react-map-gl';
 import TimelineComponent from './Timeline.js';
@@ -24,57 +28,49 @@ class App extends Component {
 
 	_onViewportChange = viewport => this.setState({viewport});
 
+	_renderMap() {
+		return (
+	        <ReactMapGL
+		        className="Map"
+		        width={1000}
+		        height={600}
+		        latitude={0}
+		        longitude={0}
+		        zoom={1}
+		        mapStyle="mapbox://styles/mapbox/dark-v9"
+		        mapboxApiAccessToken={MAPBOX_TOKEN}
+		        onViewportChange={this._onViewportChange}>
+		        
+		    </ReactMapGL>
+		)
+	}
+
+	_renderTreeMap() {
+		return (
+			<Treemap
+				title={'My New Treemap'}
+				width={1000}
+				height={600}
+				data={}
+			/>
+		)
+	}
+
 	render() {
 		return (
 		  <div className="App">
 		    <h1 className="App-title">Languages of the World</h1>
-		    <p className="App-intro">
-		      Language is a system that consists of the development, acquisition, maintenance and use of complex systems of communication, particularly the human ability to do so; and a language is any specific example of such a system.
-		The scientific study of language is called linguistics. Questions concerning the philosophy of language, such as whether words can represent experience, have been debated since Gorgias and Plato in ancient Greece. Thinkers such as Rousseau have argued that language originated from emotions while others like Kant have held that it originated from rational and logical thought. 20th-century philosophers such as Wittgenstein argued that philosophy is really the study of language. Major figures in linguistics include Ferdinand de Saussure and Noam Chomsky.
-		    </p>
-		    <div className="center">
-		        <ReactMapGL
-			        className="Map"
-			        width={1000}
-			        height={600}
-			        latitude={0}
-			        longitude={0}
-			        zoom={1}
-			        mapStyle="mapbox://styles/mapbox/dark-v9"
-			        mapboxApiAccessToken={MAPBOX_TOKEN}
-			        onViewportChange={this._onViewportChange}
-			        
-			        >
-			    </ReactMapGL>
-
-			   
+		    <div className="App-intro">
+		    	<p>The world is home to approximately 7000 living languages. In an increasingly globalized world, we find that cultural exchange and interaction becomes more commonplace. As such, the need to study foreign languages for both personal and career development rises. This project provides insight to the current linguistic landscape of the world, with the goal of increasing historical intuition and cultural awareness.</p>
+				<p>Language and culture are deeply interconnected concepts which tie in to individual identities. In the end, every individual’s identity is molded by our environment. By being cognizant of cultural diversity, we, as human beings, can further increase mutual understanding, leading to a more harmonious world.</p>
 		    </div>
-	
-          <p></p>
-          <XYPlot
-              width={300}
-              height={300}>
-              <HorizontalGridLines />
-              <LineSeries
-                color="red"
-                data={[
-                  {x: 1, y: 10},
-                  {x: 2, y: 5},
-                  {x: 3, y: 15}
-                ]}/>
-              <XAxis title="X" />
-              <YAxis />
-          </XYPlot>
 
+		    <div className="center">
+		    	{ this._renderMap() }
 
-          <TimelineComponent
-          	language="Chinese"
-          	description="The Chinese language is cool. There are many variants."
-          	events={[
-          		{"date": "800 BC", "text": "Chinese was born"},
-          		{"date": "0 AD", "text": "Jesus was born"}
-          	]}
-          	/>
+		    	{ this._renderTreeMap() }
+		    </div>
+          
       </div>
     );
   }
